f6058381dee30b6335c5a20f3f9c9ce972dc5d4e
bioinformatics <49678387+ShellyCoder@users.noreply.github.com>
2023-10-15 13:00:26 +0900

    Add files via upload

--- R/codingLncRNA/R/LncRNAData-class.R
+++ R/codingLncRNA/R/LncRNAData-class.R
@@ -12,7 +12,16 @@ setClass(
 
 #' @export
 LncRNAData <- function() {
-  data_path <- system.file("extdata", "filtered_data.csv", package = "codingLncRNA")
-  data <- fread(data_path, na.strings = c("/", "0", "NaN"))
+  data_path <- system.file("data", "filtered_data.csv", package = "codingLncRNA")
+  
+  # Read in the data without converting "0" to NA
+  data <- fread(data_path, na.strings = c("/", "NaN"))
+  
+  # Convert "0" to NA in the relevant columns after reading in the data
+  data$RNA <- ifelse(data$RNA == "0", NA, data$RNA)
+  data$Peptide <- ifelse(data$Peptide == "0", NA, data$Peptide)
+  data$ORF_seq <- ifelse(data$ORF_seq == "0", NA, data$ORF_seq)
+  
   new("LncRNAData", data = data, rna = data.frame(), peptide = data.frame(), orf = data.frame())
 }
+
--- R/codingLncRNA/R/getTestData-method.R
+++ R/codingLncRNA/R/getTestData-method.R
@@ -13,7 +13,16 @@ setGeneric("getTestData", function(object) standardGeneric("getTestData"))
 setMethod("getTestData", "LncRNAData", function(object) {
   # Adjust to your data path
   test_data_path <- system.file("extdata", "test_data.csv", package = "codingLncRNA")
-
-  # Load and return the test data
-  fread(test_data_path)
+  
+  # Load the test data
+  test_data <- fread(test_data_path, na.strings = c("/", "NaN"))
+  
+  # Convert "0" to NA in the relevant columns after reading in the data
+  test_data$RNA <- ifelse(test_data$RNA == "0", NA, test_data$RNA)
+  test_data$Peptide <- ifelse(test_data$Peptide == "0", NA, test_data$Peptide)
+  test_data$ORF_seq <- ifelse(test_data$ORF_seq == "0", NA, test_data$ORF_seq)
+  
+  # Return the adjusted test data
+  test_data
 })
+
